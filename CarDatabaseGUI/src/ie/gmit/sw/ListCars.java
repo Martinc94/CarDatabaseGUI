992c6957eecb8e6c8ed1fb86eb4bfb0ca433a982
Martinc94 <martinc94@live.co.uk>
2016-04-24 21:40:43 +0100

    removed Unnecessary Method

--- CarDatabaseGUI/src/ie/gmit/sw/ListCars.java
+++ CarDatabaseGUI/src/ie/gmit/sw/ListCars.java
@@ -2,36 +2,7 @@ package ie.gmit.sw;
 
 import java.sql.*;
 
-public class ListCars {				
-									
-	public void list(){
-		Driver d = new Driver();
-		
-		Connection conn;
-		try {
-			conn = d.getConn();
-			
-			Statement stmt = conn.createStatement();
-		
-			String qry = "select * from car";
-			ResultSet myResultSet = stmt.executeQuery(qry);
-							
-			while(myResultSet.next()){
-				System.out.println(myResultSet.getString("make")+" "
-									+myResultSet.getString("model")+" "
-									+myResultSet.getString("reg")+" "
-									+myResultSet.getString("colour")+" "
-									+myResultSet.getString("price")+" "
-									+myResultSet.getString("description"));
-			
-			}//while
-				
-			
-		} catch (SQLException e) {
-			//cannot connect to database
-		}
-		
-	}//end list
+public class ListCars {					
 	
 	public ResultSet getAllCars() throws SQLException{
 		Driver d = new Driver();
